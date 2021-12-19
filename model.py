12c987d5d0224035376772ced24da6ab6bb82c1b
MeazaEyakem1 <89281619+MeazaEyakem1@users.noreply.github.com>
2021-12-19 10:11:13 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -7,10 +7,7 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-       
         # new resolution 256*192
-
-        #new resoulution 128 * 96
         self.conv1_e1 = ConvAct(1, 64)
         self.conv1_e2 = ConvAct(64, 64)
         self.down1 = nn.MaxPool2d(2, 2)
@@ -148,7 +145,7 @@ class ConvAct(nn.Module):
             in_channels, out_channels, kernel,
             padding=padding,
             dilation=dilation)
-        self.bn = nn.BatchNorm2d(out_channels)
+        #self.bn = nn.BatchNorm2d(out_channels)
         self.activation = get_activation_by_name(activation)
         self.reset_parameters()
 
@@ -158,11 +155,11 @@ class ConvAct(nn.Module):
 
     def forward(self, x):
       x = self.conv(x)
-      x = self.bn(x)
+     # x = self.bn(x)
       return self.activation(x)
 
-
-class Discriminator(nn.Module):
+#old discriminator
+class D(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.net = nn.Sequential(
@@ -206,6 +203,127 @@ class Discriminator(nn.Module):
     def forward(self, x):
         batch_size = x.size(0)
         return torch.sigmoid(self.net(x).view(batch_size))
+
+
+#My Discriminator
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super().__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
+                    padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        #self.call_reset_parameters()
+
+        self.downsample = downsample
+        self.stride = stride
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1):
+        super().__init__()
+        
+        self.inplanes = 64
+
+        self.conv1 = nn.Conv2d(1, self.inplanes, kernel_size=3, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU()
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 , 1)
+        self.final = nn.Sigmoid()
+        
+
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None  
+   
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, 1, stride, bias=False),
+                nn.BatchNorm2d(planes),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        
+        self.inplanes = planes
+        
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        
+        for m in self.modules():
+            if isinstance(m, BasicBlock):
+                nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+                
+    
+    def forward(self, x):
+        x = self.conv1(x)           # 224x224
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)         # 112x112
+
+        x = self.layer1(x)          # 56x56
+        x = self.layer2(x)          # 28x28
+        x = self.layer3(x)          # 14x14
+        x = self.layer4(x)          # 7x7
+
+        x = self.avgpool(x)         # 1x1
+        x = torch.flatten(x, 1)     # remove 1 X 1 grid and make vector of tensor shape 
+        x = self.fc(x)
+        x = self.final(x)
+
+        return x
+
+
 
 
 class ResidualBlock(nn.Module):
