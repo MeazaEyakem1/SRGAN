1c7ae62998b26782b8ddb9c7b212a10d0334dc85
MeazaEyakem1 <89281619+MeazaEyakem1@users.noreply.github.com>
2021-12-19 10:10:36 +0100

    Add files via upload

--- loss.py
+++ loss.py
@@ -6,22 +6,33 @@ from torchvision.models.vgg import vgg16
 class GeneratorLoss(nn.Module):
     def __init__(self):
         super(GeneratorLoss, self).__init__()
-        #vgg = vgg16(pretrained=True)
-        #loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
-        #for param in loss_network.parameters():
-         #   param.requires_grad = False
-        #self.loss_network = loss_network
-        self.mse_loss = nn.MSELoss()
+        vgg = vgg16(pretrained=True)
+        loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
+        for param in loss_network.parameters():
+            param.requires_grad = False
+        self.loss_network = loss_network
+        self.mse_loss = nn.L1Loss()
         self.tv_loss = TVLoss()
 
     def forward(self, out_labels, out_images, target_images):
+
         # Adversarial Loss
         adversarial_loss = torch.mean(1 - out_labels)
-        # Perception Loss
-        #perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
-        perception_loss = self.mse_loss(out_images, target_images)
+
+        # Duplicating the gray scale image
+        out = torch.zeros_like(out_images)
+        targets = torch.zeros_like(target_images)
+        out = torch.stack((out_images,out_images,out_images), dim=2)
+        out = torch.squeeze(out)
+        targets = torch.stack((target_images,target_images,target_images), dim=2)
+        targets = torch.squeeze(targets)
+
+        # Perception loss
+        perception_loss = self.mse_loss(self.loss_network(out), self.loss_network(targets))
+
         # Image Loss
         image_loss = self.mse_loss(out_images, target_images)
+
         # TV Loss
         tv_loss = self.tv_loss(out_images)
         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
